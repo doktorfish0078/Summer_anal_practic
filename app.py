fec6818a01e4ca50b65b33b12bc6ab018e043cec
Flint01686 <44868229+Flint01686@users.noreply.github.com>
2021-06-24 00:16:45 +0400

    db

--- app.py
+++ app.py
@@ -9,6 +9,36 @@ db = SQLAlchemy(app)
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    genre = db.Column(db.String(255), nullable=False)
+    annotation = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    pageCount = db.Column(db.Integer, nullable=False)
+    roomNum = db.Column(db.Integer, nullable=False)
+    publishingHouse = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return '<Book %r>' % self.id
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    mail = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+
+class UserToBook(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, nullable=False)
+    bookId = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<UserToBook>'
+
 
 
 @app.route('/')
@@ -19,7 +49,7 @@ def home():
 
 @app.route('/book/<int:id>')
 def book(id):
-    return render_template("home.html")
+    return render_template("show_description_book.html")
 
 
 @app.route('/genres')
