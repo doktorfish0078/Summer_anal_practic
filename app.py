ab42dc9d9e3a6104a8897d3c1796e63b3c05c4f7
Flint01686 <44868229+Flint01686@users.noreply.github.com>
2021-06-23 23:23:01 +0400

    back init

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///library.db'
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "hell"
+    return render_template("home.html")
+
+
+@app.route('/book/<int:id>')
+def book(id):
+    return render_template("home.html")
+
+
+@app.route('/genres')
+def genres():
+    return render_template("genres.html")
 
 
 if __name__ == "__main__":
