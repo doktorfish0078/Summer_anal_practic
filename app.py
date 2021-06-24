e7d71e6ecb6a724d5c1c9642c80be8b3ccfc2e53
Flint01686 <44868229+Flint01686@users.noreply.github.com>
2021-06-25 02:11:48 +0400

    anal done

--- app.py
+++ app.py
@@ -91,10 +91,32 @@ def home():
     return render_template("home.html", books=books)
 
 
+def distance(a, b):
+    "Calculates the Levenshtein distance between a and b."
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n, m)) space
+        a, b = b, a
+        n, m = m, n
+
+    current_row = range(n + 1)  # Keep current and previous row, not entire matrix
+    for i in range(1, m + 1):
+        previous_row, current_row = current_row, [i] + [0] * n
+        for j in range(1, n + 1):
+            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+            if a[j - 1] != b[i - 1]:
+                change += 1
+            current_row[j] = min(add, delete, change)
+
+    return current_row[n]
+
+
 @app.route('/book/<int:id>')
 def book(id):
     book_info = Book.query.filter(Book.id == str(id)).first()
-    return render_template("show_description_book.html", book=book_info)
+    books = Book.query.filter(Book.genre == book_info.genre, Book.id != book_info.id).all()
+    books = sorted(books, key=lambda bk: distance(book_info.name, bk.name))
+    return render_template("show_description_book.html", book=book_info, books=books)
 
 
 @app.route('/genres')
